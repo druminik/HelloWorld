06bb4d7188bc431234db0ab87aa38b327f0ca781
dom <dominik_l@gmx.net>
2012-03-21 21:26:39 +0100

    initial commit

---
+++ HelloGit/src/com/dom/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.dom.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		sayHello("dude");
+	}
+
+	private static void sayHello(String name) {
+		System.out.println("Hello " + name);
+	}
+
+}
